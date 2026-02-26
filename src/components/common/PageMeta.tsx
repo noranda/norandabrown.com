f4d3e3bf38ec54104e4e338dd2df0591c0053dae
Noranda Brown <noranda@norandabrown.com>
2026-02-25 21:33:12 -0500

    Add SEO metadata, sitemap, and structured data
    - Add Open Graph and Twitter Card meta tags to index.html
    - Add JSON-LD Person schema with structured data
    - Add sitemap.xml with all 5 pages and priority weighting
    - Add robots.txt allowing all crawlers with sitemap link
    - Reorganize index.html head into logical section groups
    - Remove dead OG/Twitter/canonical tags from PageMeta (react-helmet-async v2 + React 19 only injects title)

--- src/components/common/PageMeta.tsx
+++ src/components/common/PageMeta.tsx
@@ -9,30 +9,11 @@ type PageMetaProps = {
 
 export const PageMeta = ({description, path, title}: PageMetaProps) => {
   const fullTitle = path === '/' ? title : `${title} | ${SITE_CONFIG.name}`;
-  const ogImage = `${SITE_CONFIG.baseUrl}/og-image.png`;
-  const url = `${SITE_CONFIG.baseUrl}${path}`;
 
   return (
     <Helmet>
       <title>{fullTitle}</title>
       <meta content={description} name="description" />
-      <link href={url} rel="canonical" />
-
-      {/* Open Graph */}
-      <meta content={description} property="og:description" />
-      <meta content={ogImage} property="og:image" />
-      <meta content="630" property="og:image:height" />
-      <meta content="1200" property="og:image:width" />
-      <meta content={SITE_CONFIG.name} property="og:site_name" />
-      <meta content={fullTitle} property="og:title" />
-      <meta content="website" property="og:type" />
-      <meta content={url} property="og:url" />
-
-      {/* Twitter Card */}
-      <meta content="summary_large_image" name="twitter:card" />
-      <meta content={description} name="twitter:description" />
-      <meta content={ogImage} name="twitter:image" />
-      <meta content={fullTitle} name="twitter:title" />
     </Helmet>
   );
 };
